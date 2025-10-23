248e600ce4f484c594efc13ce4972c2416d52fc2
Marc Diallo <mark.devguru@outlook.com>
2025-10-23 16:28:46 -0500

    add array of possible choices to getComputerChoice function

---
+++ script.js
@@ -0,0 +1,5 @@
+// Returns a random choice between 'rock', 'paper', or 'scissors'
+const getComputerChoice = function () {
+  // Create an array of possible choices
+  const choices = ["rock", "paper", "scissors"];
+};
