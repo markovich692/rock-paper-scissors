6936eb4051e78dbba6aedecb540c262fb63a35e7
Marc Diallo <mark.devguru@outlook.com>
2025-10-23 16:35:04 -0500

    implement random choice logic in getComputerChoice function

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
 const getComputerChoice = function () {
   // Create an array of possible choices
   const choices = ["rock", "paper", "scissors"];
+
+  const randomChoiceIndex = Math.trunc(Math.floor(Math.random() * choices.length));
+  return choices[randomChoiceIndex];
 };
